8eb4a1a7b0b9aa6cca44e59bbee415ecf8870bde
colton <51266557+coltonrusch@users.noreply.github.com>
2020-09-23 04:43:06 -0400

    Removing comments from submission because that's causing issues

---
+++ remove_comments.py
@@ -0,0 +1,32 @@
+import pyparsing
+import re
+import os
+import sys
+
+data = ''
+if len(sys.argv) - 1 == 1:
+    filename = sys.argv[1]
+
+    try:
+        with open(filename, 'r') as file:
+            data = file.read()
+
+            multiline_comment = pyparsing.nestedExpr("#|", "|#").suppress()
+            data = multiline_comment.transformString(data)
+
+            data = re.sub(r'#.*\n', '\n', data)
+
+        os.remove(filename)
+        output = open(filename, "w")
+
+        if 'provide *' not in data:
+            output.write('provide *\n')
+        if 'provide-types *' not in data:
+            output.write('provide-types *\n')
+
+        output.write(data)
+        output.close()
+    except FileNotFoundError:
+        print(f"ERROR: File {sys.argv[1]} not found.")
+else:
+    print("ERROR: python3 remove_comments.py <pyret-file.arr>")
