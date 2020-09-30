0d0af84c0cd6005e3543d72aa4469eaf400c66b6
colton <51266557+coltonrusch@users.noreply.github.com>
2020-09-30 14:35:40 -0400

    Update remove_comments.py

--- remove_comments.py
+++ remove_comments.py
@@ -4,29 +4,26 @@ import os
 import sys
 
 data = ''
-if len(sys.argv) - 1 == 1:
-    filename = sys.argv[1]
+filename = sys.argv[1]
 
-    try:
-        with open(filename, 'r', encoding="utf-8") as file:
-            data = file.read()
+try:
+    with open(filename, 'r', encoding="utf-8") as file:
+        data = file.read()
 
-            multiline_comment = pyparsing.nestedExpr("#|", "|#").suppress()
-            data = multiline_comment.transformString(data)
+        multiline_comment = pyparsing.nestedExpr("#|", "|#").suppress()
+        data = multiline_comment.transformString(data)
 
-            data = re.sub(r'#.*\n', '\n', data)
+        data = re.sub(r'#.*\n', '\n', data)
 
-        os.remove(filename)
-        output = open(filename, "w", encoding="utf-8")
+    os.remove(filename)
+    output = open(filename, "w", encoding="utf-8")
 
-        if 'provide *' not in data:
-            output.write('provide *\n')
-        if 'provide-types *' not in data:
-            output.write('provide-types *\n')
+    if 'provide *' not in data:
+        output.write('provide *\n')
+    if 'provide-types *' not in data:
+        output.write('provide-types *\n')
 
-        output.write(data)
-        output.close()
-    except FileNotFoundError:
-        print(f"ERROR: File {sys.argv[1]} not found.")
-else:
-    print("ERROR: python3 remove_comments.py <pyret-file.arr>")
+    output.write(data)
+    output.close()
+except FileNotFoundError:
+    print(f"ERROR: File {sys.argv[1]} not found.")
