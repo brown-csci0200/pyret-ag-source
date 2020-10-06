79ede696763ca481eb99010c0dffae8dc8e567a3
colton <51266557+coltonrusch@users.noreply.github.com>
2020-10-05 20:27:13 -0400

    Removed deprecated results-processing references

--- setup.sh
+++ setup.sh
@@ -8,9 +8,5 @@ curl -sL https://deb.nodesource.com/setup_8.9 -o nodesource_setup.sh
 bash nodesource_setup.sh
 apt install -y make python3 jq build-essential nodejs npm unzip
 
-# Clone result processing repo
-# npm install -g typescript ts-node
-# git clone https://github.com/brown-cs111/result-processing.git
-
 # Unpack pyret zip
 unzip "source/autograder/pyret-lang.zip" -d pyret-lang
