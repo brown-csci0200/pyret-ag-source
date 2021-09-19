3d904b1761e572d4660d59646ba6e0cf88f138bd
rsimpsonn <ryan@bonfr.com>
2021-09-19 18:00:28 -0400

    Log output

--- run_autograder.py
+++ run_autograder.py
@@ -132,8 +132,10 @@ def run(code_path, test_path, common_dir):
         # Compile test file
         try:
             compiled_tests_path = compile_tests(test_path, error)
-        except CompileError:
+        except CompileError e:
             print(f"Compilation failed: {code_path} {test_path}")
+            if hasattr(e, 'message'):
+                print(e.message)
             report_error("Compilation")
             return
 
